cfe621cb3c9f2fc203f48be8b70a9b40cb31a7ad
sajanasali123 <134501444+sajanasali123@users.noreply.github.com>
2023-06-01 23:31:07 +0530

    commited
    commited my responsive website

---
+++ Netflix/main.js
@@ -0,0 +1,55 @@
+const question1=document.getElementById("answer1");
+const questlabel1=document.getElementById("ques1")
+questlabel1.onclick = function () {
+	if(question1.style.display !== "none"){
+		question1.style.display = "none";
+	}else{
+		question1.style.display = "block";
+	}
+};
+const question2=document.getElementById("answer2");
+const questlabel2=document.getElementById("ques2")
+questlabel2.onclick = function () {
+	if(question2.style.display !== "none"){
+		question2.style.display = "none";
+	}else{
+		question2.style.display = "block";
+	}
+};
+const question3=document.getElementById("answer3");
+const questlabel3=document.getElementById("ques3")
+questlabel3.onclick = function () {
+	if(question3.style.display !== "none"){
+		question3.style.display = "none";
+	}else{
+		question3.style.display = "block";
+	}
+};
+const question4=document.getElementById("answer4");
+const questlabel4=document.getElementById("ques4")
+questlabel4.onclick = function () {
+	if(question4.style.display !== "none"){
+		question4.style.display = "none";
+	}else{
+		question4.style.display = "block";
+	}
+};
+const question5=document.getElementById("answer5");
+const questlabel5=document.getElementById("ques5")
+questlabel5.onclick = function () {
+	if(question5.style.display !== "none"){
+		question5.style.display = "none";
+	}else{
+		question5.style.display = "block";
+	}
+};
+const question6=document.getElementById("answer6");
+const questlabel6=document.getElementById("ques6")
+questlabel6.onclick = function () {
+	if(question6.style.display !== "none"){
+		question6.style.display = "none";
+	}else{
+		question6.style.display = "block";
+	}
+};
+
